893b138fb44b5bd9c690a1d1568b0c9689f86f2b
Oleg Pavlichenkov <oleg.pavlichenkov@e-legion.com>
2020-03-24 20:21:23 +0300

    Initial Commit

---
+++ openweather-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  openweather-ios
+//
+//  Created by Oleg Pavlichenkov on 24.03.2020.
+//  Copyright © 2020 just eng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
