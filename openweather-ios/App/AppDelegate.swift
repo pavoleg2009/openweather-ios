4cf11a728a2f64d1cf4f460fb59ce08c67cc911c
Oleg Pavlichenkov <oleg.pavlichenkov@e-legion.com>
2020-04-09 21:26:28 +0300

    [WIP] Move creating initial VC to DI container

--- openweather-ios/App/AppDelegate.swift
+++ openweather-ios/App/AppDelegate.swift
@@ -18,12 +18,12 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         
-//        let rootViewController = injectionContaner.makeRootViewController()
+        let rootViewController = injectionContaner.makeRootViewController()
         
         let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
         self.window = window
-        
-        AppConfigurator.configure(for: window)
         
         return true
     }
--- openweather-ios/DI/AppDependecyContaner.swift
+++ openweather-ios/DI/AppDependecyContaner.swift
@@ -8,19 +8,53 @@
 
 import UIKit
 
+//enum Tabs {
+//    case tableView
+//    case collectionView
+//    
+//    var title: String {
+//        switch self {
+//        case .tableView: return "List"
+//        case .collectionView: return "Grid"
+//        }
+//    }
+//}
+
 public class AppDependecyContaner {
     
-    init() {
-        
-    }
+    init() {}
     
     func makeRootViewController() -> UITabBarController {
         
-        return UITabBarController()
+        let tabBarViewController = UITabBarController()
+        
+        tabBarViewController.viewControllers = [
+            makeListViewController(),
+            makeGridViewController()
+        ]
+        // TODO: Jusst to show updates made to task
+        tabBarViewController.selectedIndex = 1
+        return tabBarViewController
     }
 }
 
 // MARK: - Private nethods
 private extension AppDependecyContaner {
+    func makeListViewController() -> UIViewController {
+        
+        let listNavViewController = UINavigationController()
+        listNavViewController.tabBarItem = UITabBarItem(title: "List", image: nil, selectedImage: nil)
+        let forecast5Module = Forecast5Module()
+        listNavViewController.viewControllers = [forecast5Module.viewController]
+        
+        return listNavViewController
+    }
     
+    func makeGridViewController() -> UIViewController {
+        let gridNavViewController = UINavigationController()
+        gridNavViewController.tabBarItem = UITabBarItem(title: "Grid", image: nil, selectedImage: nil)
+        let gridModule = GridModule()
+        gridNavViewController.viewControllers = [gridModule.viewController]
+        return gridNavViewController
+    }
 }
